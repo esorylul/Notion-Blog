68d07dfae635f72b334525f38f3f0abe5842d294
黄盛强 <shengqiang@192.168.1.7>
2021-02-02 23:54:52 +0800

    set nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,10 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
+  { label: '主页', page: '/' },
+  { label: '博客', page: '/blog' },
+  { label: '关于', page: '/contact' },
+  //  { label: '源码', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
@@ -31,6 +31,7 @@ export default ({ titlePre = '' }) => {
         <meta name="twitter:image" content={ogImageUrl} />
       </Head>
       <ul>
+        <h2>LulyRose</h2>
         {navItems.map(({ label, page, link }) => (
           <li key={label}>
             {page ? (
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -7,28 +7,22 @@ import contactStyles from '../styles/contact.module.css'
 import GitHub from '../components/svgs/github'
 import Twitter from '../components/svgs/twitter'
 import Envelope from '../components/svgs/envelope'
-import LinkedIn from '../components/svgs/linkedin'
 
 const contacts = [
   {
     Comp: Twitter,
     alt: 'twitter icon',
-    link: 'https://twitter.com/_ijjk',
+    link: 'https://twitter.com/esorylul',
   },
   {
     Comp: GitHub,
     alt: 'github icon',
-    link: 'https://github.com/ijjk',
-  },
-  {
-    Comp: LinkedIn,
-    alt: 'linkedin icon',
-    link: 'https://www.linkedin.com/in/jj-kasper-0b5392166/',
+    link: 'https://github.com/esorylul',
   },
   {
     Comp: Envelope,
     alt: 'envelope icon',
-    link: 'mailto:jj@jjsweb.site?subject=Notion Blog',
+    link: 'mailto:esorylul@outlook.com?subject=LulyRose',
   },
 ]
 
@@ -37,15 +31,10 @@ export default () => (
     <Header titlePre="Contact" />
     <div className={sharedStyles.layout}>
       <div className={contactStyles.avatar}>
-        <img src="/avatar.png" alt="avatar with letters JJ" height={60} />
+        <img src="/avatar.jpg" alt="avatar with LulyRose" height={60} />
       </div>
 
-      <h1 style={{ marginTop: 0 }}>Contact</h1>
-
-      <div className={contactStyles.name}>
-        JJ Kasper - Next.js Engineer @{' '}
-        <ExtLink href="https://vercel.com">Vercel</ExtLink>
-      </div>
+      <h1 style={{ marginTop: 0 }}>LulyRose</h1>
 
       <div className={contactStyles.links}>
         {contacts.map(({ Comp, link, alt }) => {
