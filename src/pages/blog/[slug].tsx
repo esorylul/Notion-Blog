2fca72cc15cf94918f17781d0b032c9ac45f3f16
esorylul <esorylul@outlook.com>
2021-02-03 22:18:30 +0800

    set Blogpage

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -153,10 +153,14 @@ const RenderPost = ({ post, redirect, preview }) => {
       <div className={blogStyles.post}>
         <h1 className={blogStyles.postName}>{post.Page || ''}</h1>
         {post.Authors.length > 0 && (
-          <div className="authors">作者: {post.Authors.join(' ')}</div>
+          <div className={blogStyles.authors}>
+            作者: {post.Authors.join(' ')}
+          </div>
         )}
         {post.Date && (
-          <div className="posted">最后更新: {getDateStr(post.Date)}</div>
+          <div className={blogStyles.posted}>
+            最后更新: {getDateStr(post.Date)}
+          </div>
         )}
 
         <hr />
